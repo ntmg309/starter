04f5a2b7e6e33b3a73df5a7e51b7bac8233b12f9
tbmg99 <rajs04011@gmail.com>
2020-05-19 11:33:33 -0700

    adding index.html and operator.js on master branch

---
+++ operator.js
@@ -0,0 +1,13 @@
+/****
+ * Basic operators
+ */ 
+var year, yearJohn, yearMark;
+now = 2020;
+ yearJohn = now - 28;
+ yearMark = now- 33;
+
+console.log(yearJohn);
+ 
+console.log (now+2);
+console.log ( now*2);
+console.log ( now/10);
